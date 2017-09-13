56ab86aefd9e44f6837168b728a5e4795f1b003f
Hansen Hansen and sravanthi velaga <hansen@>
2017-09-13 11:33:43 -0700

    create form appointment

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("form#appointment").submit(function(event){
+    var nameInput = $("input#name").val();
+    var descriptionInput = $("input#description").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".description").text(descriptionInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#confirmation").show();
+
+    event.preventDefault();
+  });
+});
